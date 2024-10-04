418b7720c5cbd44a2b39dc5da5d9fa05b242ac22
jennyrahma <jrhma26@gmail.com>
2024-10-04 15:04:04 +0700

    Update proyek_analisis_data.py

--- proyek_analisis_data.py
+++ proyek_analisis_data.py
@@ -9,12 +9,11 @@ import streamlit as st
 st.set_page_config(page_title="Bike Sharing Analysis", layout="wide")
 
 # Display project information
-st.write("# Proyek Analisis Data: Bike Sharing Dataset")
+st.markdown("<h2 style='text-align: center; color: #4CAF50;'>🚴 Proyek Analisis Data: Bike Sharing Dataset")
 st.write("**Nama:** Jenny Rahma Hidaya")
 st.write("**Email:** m191b4kx2057@bangkit.academy")
 st.write("**ID Dicoding:** jennyrhmaa")
 
-# Beautiful divider
 st.markdown("<hr style='border: 2px solid #4CAF50;'>", unsafe_allow_html=True)
 
 # Load dataset
